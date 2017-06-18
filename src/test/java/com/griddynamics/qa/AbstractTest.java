fb40ded2dcaae9a0dc0f119ae7e0410b70d27c0b
nvdoshkin <nvdoshkin@griddynamics.com>
2017-06-18 09:25:41 +0400

    Allure reporting implemented

--- src/test/java/com/griddynamics/qa/AbstractTest.java
+++ src/test/java/com/griddynamics/qa/AbstractTest.java
@@ -1,25 +1,33 @@
 package com.griddynamics.qa;
 
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import ru.yandex.qatools.allure.annotations.Step;
 
 public class AbstractTest {
 
-    @BeforeSuite
+    @BeforeSuite(alwaysRun = true)
+    @Step("Before suite")
     public void beforeSuite() {
         System.out.println("Before suite");
     }
 
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
+    @Step("After suite")
     public void afterSuite() {
         System.out.println("After suite");
     }
 
-    @BeforeMethod
+    @BeforeMethod(alwaysRun = true)
+    @Step("Before method")
     public void beforeMethod() {
         System.out.println("Before method");
     }
 
-    @AfterMethod
+    @AfterMethod(alwaysRun = true)
+    @Step("After method")
     public void afterMethod() {
         System.out.println("After method");
     }
--- src/test/java/com/griddynamics/qa/CalculatorTest.java
+++ src/test/java/com/griddynamics/qa/CalculatorTest.java
@@ -3,10 +3,13 @@ package com.griddynamics.qa;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import ru.yandex.qatools.allure.annotations.Features;
+import ru.yandex.qatools.allure.annotations.Stories;
 
 import java.lang.reflect.Method;
 
 
+@Features("Calculator testing")
 public class CalculatorTest extends AbstractTest {
     @DataProvider
     public Object[][] DataProvider(Method m) {
@@ -18,6 +21,7 @@ public class CalculatorTest extends AbstractTest {
     }
 
     @Test(dataProvider = "DataProvider", groups = "add")
+    @Stories("Add test")
     public void addTest(int a, int b) {
         System.out.println("Add test with values (" + a + ", " + b + ")");
         Assert.assertEquals(a + b, Calculator.add(a, b));
@@ -25,12 +29,14 @@ public class CalculatorTest extends AbstractTest {
 
 
     @Test(dataProvider = "DataProvider", groups = "mul")
+    @Stories("Mul test")
     public void mulTest(int a, int b) {
         System.out.println("Mul test with values (" + a + ", " + b + ")");
         Assert.assertEquals(a * b, Calculator.mul(a, b));
     }
 
     @Test(groups = {"mul", "add"})
+    @Stories("Combine test")
     public void combine() {
         System.out.println("Combine test");
         Assert.assertEquals(2 + 2 * 2, Calculator.mul(Calculator.add(2, 2), 2));
